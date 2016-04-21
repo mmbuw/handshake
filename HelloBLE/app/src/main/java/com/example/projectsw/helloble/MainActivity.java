72fbb9f7b6709e886861b5fcbfd691bc6ff2de34
Erdan Genc <erdan.genc@gmail.com>
2016-04-21 14:37:38 +0200

    wip

--- HelloBLE/app/src/main/java/com/example/projectsw/helloble/MainActivity.java
+++ HelloBLE/app/src/main/java/com/example/projectsw/helloble/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.projectsw.helloble;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,11 +19,33 @@ public class MainActivity extends AppCompatActivity {
     // http://developer.android.com/guide/topics/connectivity/bluetooth-le.html#setup
 
     private final static int REQUEST_ENABLE_BT=1;
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private TextView textView;
+    private Handler mHandler;
+    private boolean mScanning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textfield);
+
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        mHandler = new Handler();
+
+        mScanning = true;
+
+        checkBLE();
+
+        scanLeDevice(mScanning);
+    }
+
+    public void checkBLE(){
 
         // Use this check to determine whether BLE is supported on the device. Then
         // you can selectively disable BLE-related features.
@@ -32,13 +57,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.ble_supported, Toast.LENGTH_SHORT).show();
         }
 
-        TextView textView = (TextView) findViewById(R.id.textfield);
         textView.setText(R.string.ble_supported);
-
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-
-        BluetoothAdapter mBluetoothAdapter = bluetoothManager.getAdapter();
 
         // Ensures Bluetooth is available on the device and it is enabled. If not,
         // displays a dialog requesting user permission to enable Bluetooth.
@@ -47,7 +66,36 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
+    }
 
+    ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
 
+            textView.setText(result.getDevice().getName());
+
+        }
+    };
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.getBluetoothLeScanner().startScan(scanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+        }
     }
 }
