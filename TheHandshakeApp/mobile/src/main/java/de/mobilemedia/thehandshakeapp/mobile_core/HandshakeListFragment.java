a99e82d0feb47af6ed149953e8603ecab6ccfc12
Erdan Genc <erdan.genc@gmail.com>
2016-05-12 21:01:33 +0200

    adding onlongclick listener

--- TheHandshakeApp/mobile/src/main/java/de/mobilemedia/thehandshakeapp/mobile_core/HandshakeListFragment.java
+++ TheHandshakeApp/mobile/src/main/java/de/mobilemedia/thehandshakeapp/mobile_core/HandshakeListFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -38,6 +39,24 @@ public class HandshakeListFragment extends ListFragment {
                                handshakes );
 
         setListAdapter(adapter);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                HandshakeData hd = (HandshakeData) (getListAdapter()).getItem(position);
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "Sharing Handshake");
+                i.putExtra(Intent.EXTRA_TEXT, hd.getUrl());
+                startActivity(Intent.createChooser(i, "Share Handshake"));
+                return true;
+            }
+        });
     }
 
     @Override
