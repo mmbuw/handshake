800bedd60a4a404fe05dfb6f5ebd909233b6948e
sw_project <sw_project@example.com>
2016-05-30 13:53:38 +0200

    adding lineseperator, adjusting ble settings

--- TheHandshakeApp/mobile/src/main/java/de/mobilemedia/thehandshakeapp/mobile_core/Config.java
+++ TheHandshakeApp/mobile/src/main/java/de/mobilemedia/thehandshakeapp/mobile_core/Config.java
@@ -10,10 +10,10 @@ public class Config {
     public static String INITIAL_HANDSHAKE_SHORTURL = "http://bit.ly/1mmNNln";
     public static String INITIAL_HANDSHAKE_LONGURL = "http://www.binaryhexconverter.com/hex-to-decimal-converter";
 
-    public static int BLE_SCAN_PERIOD = 3000;
+    public static int BLE_SCAN_PERIOD = 4000;
     public static int BLE_TAG = 0x4343;
 
-    public static int ADVERTISE_MODE = AdvertiseSettings.ADVERTISE_MODE_BALANCED;
+    public static int ADVERTISE_MODE = AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY;
     public static int ADVERTISE_TX_POWER_LVL = AdvertiseSettings.ADVERTISE_TX_POWER_HIGH;
 
     public static String HANDSHAKE_FILE_NAME = "handshakes.map";
