c2a7eea951507405879fec315e75c2fcd28f4d1b
sw_project <sw_project@example.com>
2016-08-09 14:11:45 +0200

    visualize and modularize sorting in buckets

--- scripts/bucket_em.py
+++ scripts/bucket_em.py
@@ -2,6 +2,7 @@ import sys
 import json
 import matplotlib.pyplot as plt
 import itertools
+import numpy as np
 
 
 class Handshake(object):
@@ -18,15 +19,38 @@ class Pair(object):
 	#p_id = (unused)
 	#shake1 and shake2 = the shakes in the pair (unused)
 	#bucketdiff = bucket difference of both pairs
+	#status 0=truePositve, 1=falseNegative, 2=falsePositive, 3=trueNegative
 	p_id = ""
 	shake1 = ""
 	shake2 = ""
 	bucketdiff = 0
+	status = 0 
 
-	def __init__(self, shake1, shake2) :
+	def __init__(self, shake1, shake2, status) :
+		self.shake1=shake1
+		self.shake2=shake2
 		self.bucketdiff = abs(shake1.bucket-shake2.bucket)
+		self.status = status
 
-# maybe: Not Same Person
+	def updateBuckets(self, bucket1, bucket2):
+		self.shake1.bucket=bucket1
+		self.shake2.bucket=bucket2
+		self.bucketdiff=abs(bucket1-bucket2)
+
+# !!Not Same Person!!
+
+def is_same_person(pair):
+	try:
+		name1 = pair[0].h_id.split('-')[2].split('.')[0].strip().lower()
+	except IndexError:
+		return False
+	try:
+		name2 = pair[1].h_id.split('-')[2].split('.')[0].strip().lower()
+	except IndexError:
+		return False
+
+	return name1==name2
+
 
 def is_same_shake(pair):
 	timestamp1 = int(pair[0].h_id.split('-')[0])
@@ -40,28 +64,14 @@ def getbucket(value,num,max_value):
 	bucketsize = max_value / num
 	return int(value/bucketsize)
 
-if __name__ == '__main__':
-
-	max_value = 800
-	number_of_buckets = 8	#maybe depending on Input
-	window_size = 65		#maybe depending on Input
-
-	#List of all handshakes
+def buildPairs(data, window_size, category, categoryValue , max_value, number_of_buckets=8):
 	handshakes = []
-	#List of all valid pairs
-	pairs = []
-	#List of all fake pairs
-	fakepairs = []
 
-	data_json = sys.argv[1]
-	with open(data_json) as data_file:    
-	    data = json.load(data_file)
 	shake_ids = data[str(window_size)].keys()
 
-	#creates a List of all single sides of the handshakes
-	i = 0
+	#creates a List of all handshakes
 	for ids in shake_ids:
-		value = data["65"][ids]["fft_y"][2]
+		value = data[window_size][ids][category][categoryValue]
 		handshakes.append(Handshake(ids, getbucket(value,number_of_buckets,max_value)))
 
 	#creates all possible pairs
@@ -70,22 +80,114 @@ if __name__ == '__main__':
 	#orders shakes into valid pairs and false pairs
 	for pair in shake_id_pairs:
 		if is_same_shake(pair):
-			pairs.append(Pair(pair[0],pair[1]))
-		else:
-			fakepairs.append(Pair(pair[0],pair[1]))
+			pairs.append(Pair(pair[0],pair[1],0))
+		elif not is_same_person(pair):
+			pairs.append(Pair(pair[0],pair[1],2))
 
-	truehisto = []
+
+	return pairs
+
+def updatePairs(pairs, window_size, category, categoryValue, max_value, number_of_buckets=8):
 	for x in pairs:
-		truehisto.append(x.bucketdiff)
+		ids1 = x.shake1.h_id
+		ids2 = x.shake2.h_id
+		value1 = data[window_size][ids1][category][categoryValue]
+		value2 = data[window_size][ids2][category][categoryValue]
+		bucket1 = getbucket(value1,number_of_buckets,max_value)
+		bucket2 = getbucket(value2,number_of_buckets,max_value)
+		x.updateBuckets(bucket1,bucket2)	
+	return pairs
 
-	falsehisto = []
-	for x in fakepairs:
-		falsehisto.append(x.bucketdiff)
+def filterPairs(pairs, difftolerance=1):
+	for x in pairs:
+		if x.bucketdiff > difftolerance:
+			if x.status == 0 or x.status == 2:
+				x.status = x.status+1
+	return pairs
 
-	plt.hist(truehisto,bins=number_of_buckets, range=(0,number_of_buckets), normed = True, alpha=0.5, label='true')
-	plt.hist(falsehisto,bins=number_of_buckets, range=(0,number_of_buckets), normed = True, alpha=0.5, label='false')
-	plt.legend(loc='upper right')
+def drawHisto(pairs, number_of_buckets=8):
+	truePos =[]
+	normedLenPos = 0
+	falsePos=[]
+	normedLenNeg = 0
+	for x in pairs:
+		if x.status==0:
+			truePos.append(x.bucketdiff)
+			normedLenPos += 1
+		elif x.status==1:
+		## to visualize false negative they are visualized in bucket diff = 7 to keep the normalisation
+			truePos.append(number_of_buckets-2) 
+		elif x.status==2:
+			falsePos.append(x.bucketdiff)
+			normedLenNeg += 1
+		else:
+		## to visualize true negatives they are visualized in bucket diff = 7 to keep the normalisation
+			falsePos.append(number_of_buckets-2) 
+
+	hist, bins = np.histogram(truePos, range=(0,number_of_buckets), bins=number_of_buckets, density=True)
+	widths = np.diff(bins)
+	#hist = hist*normedLenPos* (1/float(len(truePos)))
+	plt.bar(bins[:-1], hist, widths, alpha=0.5, color='g')
+
+	hist, bins = np.histogram(falsePos, range=(0,number_of_buckets), bins=number_of_buckets, density=True)
+	widths = np.diff(bins)
+	#hist = hist*normedLenNeg* (1/float(len(falsePos)))
+	plt.bar(bins[:-1], hist, widths, alpha=0.5, color='r')
+
+	plt.ylim([0,1])
+	plt.xlim([0,number_of_buckets-1])
 	plt.show()
+
+
+if __name__ == '__main__':
+
+	#maybe depending on Input
+	max_value = 800
+	number_of_buckets = 8	
+	window_size = "65"		
+	category = "fft_y"
+	categoryValue = 2
+	difftolerance = 1 	#a tolerance of x will include all diffs from 0 to x
+
+	#List of all valid pairs
+	pairs = []
+	#List of all fake pairs
+	fakepairs = []
+	#Analysis Lists
+	truePositive = []
+	falseNegative= []
+	trueNegative = []
+	falsePositive= []
+
+	#getting the data from the json file
+	data_json = sys.argv[1]
+	with open(data_json) as data_file:    
+	    data = json.load(data_file)
+
+
+	handshakes = buildPairs(data, window_size, category, categoryValue ,max_value, number_of_buckets)
+
+	drawHisto(pairs, number_of_buckets)
+
+	handshakes = filterPairs(pairs, difftolerance)
+
+	drawHisto(pairs, number_of_buckets)
+
+	max_value = 800
+	number_of_buckets = 8	
+	window_size = "65"		
+	category = "fft_y"
+	categoryValue = 3
+	difftolerance = 1 
+
+	handshakes = updatePairs(pairs, window_size, category, categoryValue, max_value, number_of_buckets)
+
+	drawHisto(pairs, number_of_buckets)
+
+	handshakes = filterPairs(pairs, difftolerance)
+
+	drawHisto(pairs, number_of_buckets)
+
 
 ###### reminders
 #fft_y2 = [x["fft_y"][2] for x in data["65"].values()]
